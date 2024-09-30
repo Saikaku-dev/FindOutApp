7db954f3c2e2836bfb39c25b1258f884b827f7e9
Saikaku <grnmnsta@gmail.com>
2024-09-30 21:37:08 +0900

    create develop

---
+++ FindOutApp/FindOutApp/FindOutAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindOutAppApp.swift
+//  FindOutApp
+//
+//  Created by cmStudent on 2024/09/30.
+//
+
+import SwiftUI
+
+@main
+struct FindOutAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
