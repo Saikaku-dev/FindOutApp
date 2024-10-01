e2125190859da18e2abc17a5a899eb39f3456f7c
cmStudent <cmstudent@Felix.local>
2024-10-01 11:54:54 +0900

    我没改

---
+++ FindOutApp/UI/OpenView.swift
@@ -0,0 +1,20 @@
+//
+//  OpenView.swift
+//  FindOutApp
+//
+//  Created by cmStudent on 2024/10/1.
+//
+
+import SwiftUI
+@State private var isActive = false // 用于控制导航到下一个页面
+
+
+struct OpenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OpenView()
+}
