1b5c7e0dc7ebb437f97a31e487b07d5d2a313809
Saikaku-69 <grnmnsta@gmail.com>
2024-10-01 11:29:12 +0900

    Add files via upload

---
+++ FindOutApp/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  FindOutApp
+//
+//  Created by cmStudent on 2024/09/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var MoveToMapView:Bool = false
+    var body: some View {
+        VStack {
+            Button(action : {
+                MoveToMapView = true
+            }) {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+            }
+            Text("Hello, world!")
+        }
+        .padding()
+        .fullScreenCover(isPresented: $MoveToMapView) {
+            ScaleMapView()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
