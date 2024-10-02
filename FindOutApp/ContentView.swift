99f320af3b2771d7f1390e2af6be1dc1e068c998
cmStudent <cmstudent@Felix.local>
2024-10-02 13:48:16 +0900

    no add

--- FindOutApp/ContentView.swift
+++ FindOutApp/ContentView.swift
@@ -22,7 +22,7 @@ struct ContentView: View {
         }
         .padding()
         .fullScreenCover(isPresented: $MoveToMapView) {
-            ScaleMapView()
+            BaseMapView()
         }
     }
 }
