9ddc26262fdbc40b5d509196c6cfc3b288918303
Saikaku <grnmnsta@gmail.com>
2024-10-31 16:12:16 +0900

    edit gauge

--- FindOutApp/Main/GameTimeCountView.swift
+++ FindOutApp/Main/GameTimeCountView.swift
@@ -11,20 +11,31 @@ struct GameTimeCountView: View {
     @ObservedObject var gameTime = GameTime.shared
     var body: some View {
         ZStack {
+//            Color.black.ignoresSafeArea()
             HStack {
                 Gauge(value: Double(gameTime.countTime), in: 0...60) {
                     //
                 }
                 .progressViewStyle(LinearProgressViewStyle())
-                .tint(LinearGradient(colors: [.red,.orange,.yellow], startPoint: .leading, endPoint: .trailing))
+                .tint(LinearGradient(colors: [.red,.orange,.yellow,.green,.green], startPoint: .leading, endPoint: .trailing))
                 .frame(width:UIScreen.main.bounds.width/2)
                 .overlay(
                     RoundedRectangle(cornerRadius:15)
                         .stroke(Color.black,lineWidth: 2)
                 )
-                Text("\(GameTime.shared.countTime)")
-                    .fontWeight(.bold)
-                    .font(.title)
+                ZStack {
+                    Image(systemName: "alarm")
+                    Circle()
+                        .fill(.white)
+                        .frame(width: 20)
+                    Text("\(GameTime.shared.countTime)")
+                        .fontWeight(.bold)
+                        .font(.system(size:13))
+                }
+                .font(.system(size:30))
+                .background(.white)
+                .cornerRadius(15)
+                
             }
             .frame(width:UIScreen.main.bounds.width)
         }
